579f26351d18427aa1fc87cac15b3c5cb9cb62fc
Smorodin Sergey <ssmorodin@bk.ru>
2025-06-26 14:31:47 +0300

    Игра-кликер 2

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,9 +1,14 @@
+const cookie = document.getElementById('cookie');
 const counter = document.getElementById('clicker__counter');
+const speedElement = document.getElementById('speed__counter');
 
 let clickCount = 0;
 let isCookieOriginal = false;
+let lastClickTime = null;
 
 cookie.onclick = function() {
+  const currentTime = new Date();
+
   clickCount++;
   counter.textContent = clickCount;
 
@@ -12,6 +17,13 @@ cookie.onclick = function() {
   } else {
     cookie.width = 220;
   }
-
   isCookieOriginal = !isCookieOriginal;
+
+  if (lastClickTime !== null) {
+    const timeDiff = (currentTime - lastClickTime) / 1000;
+    const clicksPerSecond = (1 / timeDiff).toFixed(2);
+    speedElement.textContent = clicksPerSecond;
+  }
+
+  lastClickTime = currentTime;
 };
